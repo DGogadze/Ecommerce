abd2e8796d7d8cc55bf1721e1d3157e0786dbd99
LordCommander21 <gogadzedaniel21@gmail.com>
2020-10-05 12:14:30 +0400

    initialize

---
+++ src/test/java/ge/eccomerce/eccomerce/EccomerceApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.eccomerce.eccomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EccomerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
