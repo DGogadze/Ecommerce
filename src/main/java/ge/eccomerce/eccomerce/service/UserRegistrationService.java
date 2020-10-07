c5fc4ec6f111750a612194f1923a5ba8cfed6070
LordCommander21 <gogadzedaniel21@gmail.com>
2020-10-07 11:45:48 +0400

    addedMailSender

--- src/main/java/ge/eccomerce/eccomerce/service/UserRegistrationService.java
+++ src/main/java/ge/eccomerce/eccomerce/service/UserRegistrationService.java
@@ -9,9 +9,15 @@ import org.springframework.stereotype.Service;
 public class UserRegistrationService {
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    MailSenderService mailSenderService;
 
     public void userRegistration(String userEmail,String userName,String userLastname,int userPrivateId){
         User user = new User(userPrivateId,userName,userLastname,userEmail);
+        mailSenderService.sendMail(userEmail,"Ec-commerce registration",
+                "\t მოგესალმებით " + userName + " " + userLastname + "\n თქვენ დარეგისტრირდით" +
+                        " ონლაინ მაღაზია Ec-commerce-ში.\n გაყევით ბმულს თქვენი მომხმარებლის აქტივაციისთვის." +
+                        "\n SHORTLINK HERE");
         userRepository.save(user);
     }
 }
