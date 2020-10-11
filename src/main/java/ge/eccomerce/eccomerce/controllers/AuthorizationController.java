bae272147b34c70f764ff7ac60b6195e9d03e9f0
LordCommander21 <gogadzedaniel21@gmail.com>
2020-10-11 20:49:16 +0400

    userViewsAndSyntaxFixes

--- src/main/java/ge/eccomerce/eccomerce/controllers/AuthorizationController.java
+++ src/main/java/ge/eccomerce/eccomerce/controllers/AuthorizationController.java
@@ -4,18 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthorizationController {
     @GetMapping("/login")
-    public String login(Model model){
+    public String login(){
         return "authorization";
     }
     @PostMapping("/login")
-    public String login(@RequestParam String userEmail,
-                        @RequestParam String userPassword,
-                        Model model){
-        return "redirect:/profile";
+    public String login(Model model){
+        return "authorization";
     }
 }
