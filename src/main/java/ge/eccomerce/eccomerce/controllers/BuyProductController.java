a4d6fff6b81387a5f75fa2d048eb96db49f8245b
LordCommander21 <gogadzedaniel21@gmail.com>
2020-10-19 11:25:01 +0400

    needToFixBuyProductForm

--- src/main/java/ge/eccomerce/eccomerce/controllers/BuyProductController.java
+++ src/main/java/ge/eccomerce/eccomerce/controllers/BuyProductController.java
@@ -1,12 +1,23 @@
 package ge.eccomerce.eccomerce.controllers;
 
+import ge.eccomerce.eccomerce.model.Product;
+import ge.eccomerce.eccomerce.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BuyProductController {
+    @Autowired
+    ProductRepository productRepository;
+
     @PostMapping("/buyproduct")
-    public String buyProduct(int count){
-        return "redirect:/home";
+    public String buyProduct(@RequestParam int count,@RequestParam long id){
+        Product product = productRepository.findById(id);
+        int activeCount = product.getCount();
+        product.setCount(activeCount-=count);
+        productRepository.save(product);
+        return "redirect:/";
     }
 }
