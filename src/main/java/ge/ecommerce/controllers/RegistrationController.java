207b1e436e1fe167ab22f5137c4dee8f2e8cc404
LordCommander21 <gogadzedaniel21@gmail.com>
2021-07-16 20:58:11 +0400

    small fixes

--- src/main/java/ge/ecommerce/controllers/RegistrationController.java
+++ src/main/java/ge/ecommerce/controllers/RegistrationController.java
@@ -1,13 +1,13 @@
-package ge.eccomerce.eccomerce.controllers;
+package ge.ecommerce.controllers;
 
-import ge.eccomerce.eccomerce.model.User;
-import ge.eccomerce.eccomerce.repository.UserRepository;
-import ge.eccomerce.eccomerce.service.UserRegistrationService;
+import ge.ecommerce.model.User;
+import ge.ecommerce.repository.UserRepository;
+import ge.ecommerce.service.UserRegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class RegistrationController {
@@ -18,15 +18,15 @@ public class RegistrationController {
     UserRepository userRepository;
 
     @GetMapping("/registration")
-    public String registration(){
+    public String registration() {
         return "registration";
     }
 
     @PostMapping("/registration")
-    public String registration(@RequestBody User user){
+    public String registration(@RequestBody User user) {
         String userEmail = user.getUserEmail();
         String userPrivateId = user.getUserPrivateId();
-        if (userRepository.findByUserEmail(userEmail)==null&&userRepository.findByUserPrivateId(userPrivateId)==null){
+        if (userRepository.findByUserEmail(userEmail) == null && userRepository.findByUserPrivateId(userPrivateId) == null) {
             userRegistrationService.userRegistration(user);
         } else {
             return "redirect:/registrationfailed";
@@ -36,7 +36,7 @@ public class RegistrationController {
     }
 
     @GetMapping("/registrationfailed")
-    public String registrationFailed(){
+    public String registrationFailed() {
 
         return "registrationfailed";
     }
